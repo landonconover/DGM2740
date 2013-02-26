47f2677794610791b0693fe7a9c6f419a62afc8e
Landon Conover <landon.conover@uvu.edu>
2013-02-25 17:04:43 -0700

    Init commit for assign 6

---
+++ 06PHP/about.php
@@ -0,0 +1,74 @@
+<?php 
+$page = 'about';
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
+    <title>About Us</title>
+
+<?php include 'includes.php'; ?>
+
+</head>
+<body>
+    <div id="container">
+        <?php include 'header.php'; ?>
+
+        <div id="prettyBar">
+            <div id="prettyLeft">
+                <div id="prettyContent"></div>
+            </div>
+        </div>
+
+        <div id="nav-contain">
+            <div id="nav-side">
+                <div id="nav-middle">
+                    <?php include 'menu.php'; ?>
+                </div>
+            </div>
+        </div>
+
+        
+
+        <div id="main">
+            <div id="left_col">
+                <article>
+                    <h3>Sewing</h3>
+                    <hr>
+                    <figure class="floatMe"><img src="http://placehold.it/350x150">
+                    <figcaption>Caption</figcaption>
+                    </figure>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque iure minima soluta delectus aspernatur eaque ab cum ullam error. Quisquam pariatur reiciendis recusandae molestias iste accusantium suscipit dolorem non laborum.
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque iure minima soluta delectus aspernatur eaque ab cum ullam error. Quisquam pariatur reiciendis recusandae molestias iste accusantium suscipit dolorem non laborum.
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque iure minima soluta delectus aspernatur eaque ab cum ullam error. Quisquam pariatur reiciendis recusandae molestias iste accusantium suscipit dolorem non laborum.
+
+                    </p>
+                    <div class="clear"></div>
+                </article>
+                <article>
+                    <h3>Sewing</h3>
+                    <hr>
+                    <figure class="floatMe"><img src="http://placehold.it/350x150">
+                    <figcaption>Caption</figcaption>
+                    </figure>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque iure minima soluta delectus aspernatur eaque ab cum ullam error. Quisquam pariatur reiciendis recusandae molestias iste accusantium suscipit dolorem non laborum.
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque iure minima soluta delectus aspernatur eaque ab cum ullam error. Quisquam pariatur reiciendis recusandae molestias iste accusantium suscipit dolorem non laborum.
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque iure minima soluta delectus aspernatur eaque ab cum ullam error. Quisquam pariatur reiciendis recusandae molestias iste accusantium suscipit dolorem non laborum.
+
+                    </p>
+                    <div class="clear"></div>
+                </article>
+            </div>
+            
+        </div>
+
+        <?php include 'footer.php'; ?>
+
+
+    </div>
+</body>
+</html>
---
+++ 06PHP/includes.php
@@ -0,0 +1,6 @@
+    <link href='http://fonts.googleapis.com/css?family=Rambla:400,700' rel='stylesheet' type='text/css'>
+
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/phone.css">
+    <link rel="stylesheet" href="css/tablet.css">
+    <link rel="stylesheet" href="css/desktop.css">
