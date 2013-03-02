145268f03b372b62602581566c306d71e5eb0499
Landon Conover <landon.conover@uvu.edu>
2013-03-02 14:37:57 -0700

    O7 Forms Assign no script

--- 07Forms/includes.php
+++ 07Forms/includes.php
@@ -5,4 +5,3 @@
     <link rel="stylesheet" href="css/tablet.css">
     <link rel="stylesheet" href="css/desktop.css">
 
-    <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
