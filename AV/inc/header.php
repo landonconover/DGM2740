4c4f3565c609c62d073075b3be8e247a8d2faf90
Landon Conover <landonconover@dhcp-161-28-122-60.ad.uvu.edu>
2013-04-22 16:54:05 -0600

    Testing

--- AV/inc/header.php
+++ AV/inc/header.php
@@ -4,11 +4,9 @@
         <a class="alt <?php if($page == 'book'){echo 'active';} ?>" href="book.php">Book Now</a>
 				<a class="alt <?php if($page == 'about'){echo 'active';} ?>" href="about.php">About</a>
         <a class="alt <?php if($page == 'contact'){echo 'active';} ?>" href="contact.php">Contact</a>
-        <!--[if lt IE 7 ]> <div class="logocontain"><a class="alt logo" href="index.php"><img class='logoimg' src="Audio-Visions-WebIE.png" alt="logo"></a> </div><![endif]-->
-        <!--[if IE 7 ]>    <div class="logocontain"><a class="alt logo" href="index.php"><img class='logoimg' src="Audio-Visions-WebIE.png" alt="logo"></a> </div><![endif]-->
+        
         <!--[if IE 8 ]>    <a class="alt logo" href="index.php"><img class='logoimg' src="Audio-Visions-WebIE.png" alt="logo"></a> <![endif]-->
-        <!--[if IE 9 ]>     <a class="alt logo" href="index.php">Audio Visions Mobile DJ</a> <![endif]-->
-        <!--[if (gt IE 9)|!(IE)]><!--> <a class="alt logo" href="index.php">Audio Visions Mobile DJ</a> <!--<![endif]-->
+        
         <a class="alt <?php if($page == 'comment'){echo 'active';} ?>" href="comment.php">Comment</a>
         <a class="alt <?php if($page == 'photos'){echo 'active';} ?>" href="photos.php">Photos</a>
         <a class="alt <?php if($page == 'music'){echo 'active';} ?>" href="music.php">Music</a>
